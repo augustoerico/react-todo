9c840a18e03a38e8265a3d4e4ecbd15cd8111c5c
Erico Silva <augusto.ericosilva@gmail.com>
2017-03-25 14:15:07 -0300

    Add test for TaskList component

--- app/tests/components/TaskList.test.jsx
+++ app/tests/components/TaskList.test.jsx
@@ -5,11 +5,23 @@ var expect = require('expect');
 var $ = require('jquery');
 
 var TaskList = require('TaskList');
+var Task = require('Task');
 
 describe('TaskList', () => {
 
     it('should exist', () => {
         expect(TaskList).toExist();
+    });
+
+    it('should render one Task componet for each task item', () => {
+        var tasks = [
+            {id: 1, text: 'foo'},
+            {id: 2, text: 'bar'}
+        ]
+        var taskList = TestUtils.renderIntoDocument(<TaskList tasks={tasks} />);
+        var taskComponents = TestUtils.scryRenderedComponentsWithType(taskList, Task);
+
+        expect(taskList.lenght).toBe(tasks.lenght);
     });
 
 });
