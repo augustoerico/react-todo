15820cd567c60d0b442e27af7ebe5be9b8537034
Erico Silva <augusto.ericosilva@gmail.com>
2017-03-25 13:47:50 -0300

    Add list rendering

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
 
+var TaskList = require('TaskList');
+
 var App = React.createClass({
+    getInitialState: function() {
+        return {
+            tasks: [
+                {id: 1, text: 'Play video-game'},
+                {id: 2, text: 'Drink coffee'}
+            ]
+        }
+    },
     render: function () {
+        var {tasks} = this.state;
         return (
             <div>
                 App.jsx
+                <TaskList tasks={tasks} />
             </div>
         )
     }
