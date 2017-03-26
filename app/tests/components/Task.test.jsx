65ab3b58ed2b26b5ec3b35994a9e044a0851c562
Erico Silva <augusto.ericosilva@gmail.com>
2017-03-25 21:44:39 -0300

    Add test

--- app/tests/components/Task.test.jsx
+++ app/tests/components/Task.test.jsx
@@ -12,4 +12,19 @@ describe('Task', () => {
         expect(Task).toExist();
     });
 
+    it('should call onToggle with id on change', () => {
+        var task = {
+            id: 200,
+            text: 'Write Task test',
+            completed: false
+        };
+
+        var spy = expect.createSpy();
+        var task = TestUtils.renderIntoDocument(<Task {...task} onToggle={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(task));
+
+        TestUtils.Simulate.change($el.find('input')[0]);
+        expect(spy).toHaveBeenCalledWith(200);
+    });
+
 });
