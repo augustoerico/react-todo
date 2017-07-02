97b4e711da5fca61340b6a20bfccc4da40461a73
Erico Silva <augusto.ericosilva@gmail.com>
2017-07-01 22:25:08 -0300

    Add toggle task

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchReducer = (state = '', action) => {
     switch (action.type) {
         case 'SET_SEARCH':
@@ -11,6 +14,36 @@ export var showCompletedReducer = (state = false, action) => {
     switch (action.type) {
         case 'TOGGLE_SHOW_COMPLETED':
             return !state;
+        default:
+            return state;
+    }
+};
+
+export var tasksReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TASK':
+            return [
+                ...state,
+                {
+                    id: uuid(),
+                    text: action.text,
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ];
+        case 'TOGGLE_TASK':
+            return state.map((task) => {
+                if (task.id === action.id) {
+                    var completed = !task.completed;
+                    return {
+                        ...task,
+                        completed: completed,
+                        completedAt: completed ? moment().unix() : undefined
+                    };
+                }
+                return task;
+            });
         default:
             return state;
     }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 var expect = require('expect');
 var deepFreeze = require('deep-freeze-strict');
+var moment = require('moment');
 
 var reducers = require('reducers');
 
@@ -35,6 +36,52 @@ describe('Reducers', () => {
             result = reducers.showCompletedReducer(deepFreeze(result), deepFreeze(action));
 
             expect(result).toEqual(false);
+        });
+
+    });
+
+    describe('tasksReducer', () => {
+        
+        it('should add a task', () => {
+
+            var action = {
+                type: 'ADD_TASK',
+                text: 'Do something'
+            }
+
+            var result = reducers.tasksReducer(deepFreeze([]), deepFreeze(action));
+
+            expect(result.length).toEqual(1);
+            expect(result[0].text).toEqual(action.text);
+
+        });
+
+        it('should toggle a task', () => {
+            var action = {
+                type: 'TOGGLE_TASK',
+                id: '1234'
+            }
+            var tasks = [
+                {
+                    id: '1234',
+                    text: 'Learn Redux',
+                    completed: false,
+                    createdAt: 1234,
+                    completedAt: undefined
+                }
+            ];
+
+            var result = reducers.tasksReducer(deepFreeze(tasks), deepFreeze(action));
+
+            expect(result.length).toEqual(1);
+            expect(result[0].completed).toEqual(true);
+            expect(result[0].completedAt).toNotEqual(undefined);
+
+            result = reducers.tasksReducer(deepFreeze(result), deepFreeze(action));
+            
+            expect(result[0].completed).toEqual(false);
+            expect(result[0].completedAt).toEqual(undefined);
+
         });
 
     });
